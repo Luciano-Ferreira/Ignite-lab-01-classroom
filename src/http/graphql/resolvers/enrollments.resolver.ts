e38a540d6f35a9782ce96296cd6f5fbdc03f7df5
Luciano Ferreira <luciano2023silva@gmail.com>
2022-04-27 23:39:06 -0300

    feat: ResolveField of enrollments

--- src/http/graphql/resolvers/enrollments.resolver.ts
+++ src/http/graphql/resolvers/enrollments.resolver.ts
@@ -1,16 +1,33 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { Enrollment } from '../models/enrollment';
 import { EnrollmentService } from '../../../services/enrollments.service';
+import { Course } from '../models/course';
+import { CoursesService } from '../../../services/courses.service';
+import { StudentsService } from '../../../services/students.service';
 
 @Resolver(() => Enrollment)
 export class EnrollmentsResolver {
-  constructor(private enrollmentsService: EnrollmentService) {}
+  constructor(
+    private enrollmentsService: EnrollmentService,
+    private coursesService: CoursesService,
+    private studentsService: StudentsService,
+  ) {}
 
   @Query(() => [Enrollment])
   @UseGuards(AuthorizationGuard)
   enrollments() {
     return this.enrollmentsService.listAllEnrollments();
+  }
+
+  @ResolveField()
+  student(@Parent() enrollment: Enrollment) {
+    return this.studentsService.getStudentById(enrollment.studentId);
+  }
+
+  @ResolveField()
+  course(@Parent() enrollment: Enrollment) {
+    return this.coursesService.getCourseById(enrollment.courseId);
   }
 }
