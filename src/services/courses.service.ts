76ede2e413d41b4772b2ab4321484869bd5de25a
Luciano Ferreira <luciano2023silva@gmail.com>
2022-04-29 14:18:30 -0300

    feat: createCourse route

--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
-
+import slugify from 'slugify';
+interface CreateCourseParamas {
+  title: string;
+}
 @Injectable()
 export class CoursesService {
   constructor(private prisma: PrismaService) {}
@@ -13,6 +16,27 @@ export class CoursesService {
     return this.prisma.course.findUnique({
       where: {
         id,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParamas) {
+    const slug = slugify(title, { lower: true });
+
+    const courseAllReadyExists = await this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (courseAllReadyExists) {
+      throw new Error('Course already');
+    }
+
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
       },
     });
   }
